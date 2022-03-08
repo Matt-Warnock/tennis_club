1943c9ebc908a4e4f25488510f6902408db5eb35
MattW <mattnwarnock@yahoo.co.uk>
2022-03-08 13:52:07 +0000

    Route sends valid JSON response

--- spec/tennis_club_spec.rb
+++ spec/tennis_club_spec.rb
@@ -8,10 +8,25 @@ RSpec.describe 'tennis_club' do
   end
 
   describe '/v1/players' do
-    it 'is successful' do
-      post '/v1/players'
+    before { post '/v1/players' }
 
+    it 'is successful' do
       expect(last_response).to be_ok
     end
+
+    it 'sends correct content type' do
+      expect(last_response.headers['Content-Type']).to eq 'application/json'
+    end
+
+    it 'sends valid_json' do
+      expect(valid_json?(last_response.body)).to be true
+    end
+  end
+
+  def valid_json?(json)
+    JSON.parse(json)
+    true
+  rescue JSON::ParserError
+    false
   end
 end
--- tennis_club.rb
+++ tennis_club.rb
@@ -3,6 +3,11 @@
 require 'sinatra'
 
 class TennisClub < Sinatra::Base
+  before do
+    content_type :json
+  end
+
   post '/v1/players' do
+    { player: 'added to database' }.to_json
   end
 end
