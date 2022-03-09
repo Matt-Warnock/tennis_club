c94d00561c042852b3450769855e827631aa9d21
MattW <mattnwarnock@yahoo.co.uk>
2022-03-09 17:21:06 +0000

    Refactor admin for srp and encapsulation

--- lib/administrator.rb
+++ lib/administrator.rb
@@ -6,16 +6,27 @@ class Administrator
   end
 
   def register_player(player)
-    return raise 'bad or incomplete data' unless all_data_present?(player)
-
-    return raise 'player already registered' unless @players.find(player[:first_name], player[:last_name]).empty?
+    check_data(player)
+    check_existing_player(player)
 
     @players.create(player)
+  end
+
+  private
+
+  def check_data(player)
+    raise 'bad or incomplete data' unless all_data_present?(player)
   end
 
   def all_data_present?(player)
     required_keys = %i[first_name last_name nationality birth_date]
 
     required_keys.all? { |key| player.key?(key) }
+  end
+
+  def check_existing_player(player)
+    player_search = @players.find(player[:first_name], player[:last_name])
+
+    raise 'player already registered' unless player_search.empty?
   end
 end
